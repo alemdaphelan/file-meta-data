347124fb56b19ae0937ee3e14e9221375f5f0c62
Alem <lamto20050505@gmail.com>
2025-06-27 10:25:01 +0700

    complete

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var cors = require('cors');
 require('dotenv').config()
-
+var multer = require('multer');
 var app = express();
 
+let storage = multer.memoryStorage();
+let upload = multer({storage:storage});
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
 
@@ -11,7 +13,11 @@ app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-
+app.post('/api/fileanalyse',upload.single('upfile'),(req,res)=>{
+  const {originalname,mimetype,size} = req.file;
+  const name = Buffer.from(originalname,'latin1').toString('utf-8');
+  res.json({name: name, type: mimetype, size: size});
+})
 
 
 const port = process.env.PORT || 3000;
